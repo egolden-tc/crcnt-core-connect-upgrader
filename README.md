1f80ed96a16cf1fd85e500b9dc5bf7f61102f8ba
erigolden <egolden-tc@users.noreply.github.com>
2021-10-20 15:15:01 -0400

    Additional changes.

--- README.md
+++ README.md
@@ -4,15 +4,20 @@ This application is used for upgrading customer sandboxes to the latest Core Con
 
 ## Instructions
 
-1. Authorize the Core Connect DevHub using the following command:
+1. Start by cloning this repo locally and navigating into that folder
+   ```
+    git clone https://github.com/egolden-tc/crcnt-core-connect-upgrader.git
+    cd crcnt-core-connect-upgrader
+   ```
+2. Authorize the Core Connect DevHub using the following command:
    ```
     sfdx force:auth:web:login -a devhub
    ```
-2. Once that's authorized, run the following command to authorize the target customer sandbox
+3. Once that's authorized, run the following command to authorize the target customer sandbox
    ```
    sfdx force:auth:web:login -r https://test.salesforce.com -a sandbox
    ```
-3. After both orgs are authorized, you can run the following command to execute the script
+4. After both orgs are authorized, you can run the following command to execute the script
    ```
    node index.js
    ```
--- index.js
+++ index.js
@@ -1,7 +1,82 @@
 const sfdx = require("sfdx-node");
+const fs = require("fs");
+const xml2js = require("xml2js");
 
-// retrieve latest account page and backup
+const SANDBOX_ALIAS = "chcd71";
 
-// deploy original account page
+const generateCustomMetadataXml = () => {
+  return new Promise((resolve, reject) => {
+    const exportedMetadata = require("./customMetadata.json");
+    const dcoreMetadata = exportedMetadata.filter(row => {
+      return (
+        row.fullName.startsWith("dcorealpha__") &&
+        row.namespacePrefix !== "dcorealpha"
+      );
+    });
 
-// retrieve all non-namespaced Cards
+    fs.readFile("metadataPackage.xml", "utf-8", (err, data) => {
+      if (err) reject(err);
+      xml2js.parseString(data, (parseErr, result) => {
+        if (err) reject(parseErr);
+        result.Package.types[0].members = dcoreMetadata.map(item => {
+          return item.fullName;
+        });
+        const builder = new xml2js.Builder();
+        const builtXml = builder.buildObject(result);
+        fs.writeFile(
+          "metadataPackage.xml",
+          builtXml,
+          (writeErr, writtenData) => {
+            if (writeErr) reject(writeErr);
+            fs.writeFile(
+              "temp/destructiveChanges.xml",
+              builtXml,
+              (writeErr2, writtenData2) => {
+                if (writeErr2) reject(writeErr2);
+                resolve(true);
+              }
+            );
+          }
+        );
+      });
+    });
+  });
+};
+
+(async () => {
+  // retrieve latest account page and backup
+
+  // deploy original account page
+  // retrieve all non-namespaced Cards
+  console.log("Retrieving list of Core Connect metadata to backup.");
+  await sfdx.force.mdapi.listmetadata({
+    resultfile: "customMetadata.json",
+    json: true,
+    targetusername: SANDBOX_ALIAS,
+    metadatatype: "CustomMetadata"
+  });
+  console.log("Generating XML for custom metadata");
+  await generateCustomMetadataXml();
+  // backup custom metadata
+  console.log("Backing up metadata");
+  await sfdx.force.mdapi.retrieve({
+    unpackaged: "temp/destructiveChanges.xml",
+    retrievetargetdir: "./customMetadataBackup",
+    targetusername: SANDBOX_ALIAS
+  });
+  // delete custom metadata
+  console.log("deleting metadata");
+  await sfdx.force.mdapi.deploy({
+    wait: -1,
+    targetusername: SANDBOX_ALIAS,
+    deploydir: "temp"
+  });
+  // deploy non-namespaced cards
+  console.log("deploying backed up metadata");
+  await sfdx.force.mdapi.deploy({
+    wait: -1,
+    targetusername: SANDBOX_ALIAS,
+    zipfile: "./customMetadataBackup/unpackaged.zip",
+    _quiet: false
+  });
+})();
