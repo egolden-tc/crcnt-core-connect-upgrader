5852e688505e7b989ca3f9ef3a9c6578bca2367f
erigolden <egolden-tc@users.noreply.github.com>
2021-11-23 08:09:00 -0500

    Updated package id.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const execSync = require("child_process").execSync;
 const IS_WINDOWS = process.platform === "win32";
 
 const SANDBOX_ALIAS = "humana";
-const NEW_PACKAGE_ID = "04t5e000000Jkj4AAC";
+const NEW_PACKAGE_ID = "04t5e000000Jko0AAC";
 const NAMESPACE = "dcorealpha";
 const INSTALL_KEY = "";
 const PACKAGE_NAME = "CoreConnectAlpha";
